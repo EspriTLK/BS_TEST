a3798c918b7440ed999da5ee2a4667edf343ed8d
EspriTLK <esprit@inbox.ru>
2023-03-02 16:14:25 +0300

    refactor: change findOne2

--- src/api/publication/controllers/publication.js
+++ src/api/publication/controllers/publication.js
@@ -45,36 +45,100 @@ module.exports = createCoreController(publicationApi, ({ strapi }) => ({
 	},
 
 	async findOne(ctx) {
-		const { query, state } = ctx
-		const params = {
-			...ctx,
-			query: {
-				...query,
-				populate: { author: true }
+		// some logic here
+		// const qp = await this.sanitizeParams(ctx);
+		const { params, state } = ctx
+		const { id } = params
+		const currentUser = state.user
+		const queryParams = {
+			where: {
+				$and: [
+					{ id },
+					{ publishedAt: { $notNull: true } }
+				]
 			}
 		}
 
-		try {
-			const { data } = await super.findOne(params)
-			const { attributes } = data
-			const { publishedAt, author } = attributes
+		if (currentUser) {
+			const { id } = currentUser
+			queryParams.where = {
 
-			const currentUser = state.user
-			const isPublished = publishedAt !== null
-			const isAuthor = currentUser?.id === author.data.id
-			const isEditor = currentUser?.role.name === 'Editor'
-
-			if (!isPublished && (!currentUser || !isAuthor) && !isEditor) {
-				ctx.forbidden('You are not allowed to view this publication')
+				$and: [
+					{ id },
+					{
+						$or:
+							[
+								{ author: currentUser },
+								{ author: { role: { name: 'Author' } } }
+							]
+					}
+				]
 			}
-
-			return await super.findOne(ctx);
-		} catch {
-
-			return
 		}
 
+
+		const publication = await strapi.db.query(publicationApi).findOne(queryParams);
+		const sanitizedResults = await this.sanitizeOutput(publication, ctx);
+		console.log(publication);
+		// console.log(ctx.state.user.role.id);
+
+		return this.transformResponse(sanitizedResults);
+
+
+		// const { id } = ctx.params
+
+		// const { publishedAt, author } = await strapi.entityService.findOne(publicationApi, id, { populate: { author: true } })
+
+		// const currentUser = ctx.state.user
+		// const isPublished = publishedAt !== null
+		// const isAuthor = currentUser?.id === author.id
+		// const isEditor = currentUser?.role.name === 'Editor'
+
+		// if (!isPublished && (!currentUser || !isAuthor) && !isEditor) {
+		// 	ctx.throw(403, 'You are not allowed to edit this publication')
+		// }
+
+		// return await super.findOne(ctx);
 	},
+
+	// async findOne(ctx) {
+	// 	// some logic here
+	// 	if (!ctx?.params) {
+	// 		return
+	// 	}
+
+	// 	const { id } = ctx.params
+
+	// 	const { publishedAt, author } = await strapi.entityService.findOne(publicationApi, id, { populate: { author: true } })
+
+	// 	const currentUser = ctx.state.user
+	// 	const isPublished = publishedAt !== null
+	// 	const isAuthor = currentUser?.id === author.id
+	// 	const isEditor = currentUser?.role.name === 'Editor'
+
+	// 	if (!isPublished && (!currentUser || !isAuthor) && !isEditor) {
+	// 		ctx.throw(403, 'You are not allowed to edit this publication')
+	// 	}
+
+	// 	return await super.findOne(ctx);
+	// },
+
+	// async findOne(ctx) {
+	// 	// some logic here
+	// 	const currentUser = ctx.state.user
+
+	// 	ctx.query = {
+	// 		...ctx.query,
+	// 		populate: { author: true }
+	// 	}
+
+
+	// 	let resp = await super.findOne(ctx)
+	// 	resp.data.attributes.author = undefined
+
+	// 	return resp
+
+	// },
 
 	async create(ctx) {
 		// some logic here
