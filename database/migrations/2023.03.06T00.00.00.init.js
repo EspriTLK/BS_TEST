23f238a38884669c7468f5888e1943a28ad4df8f
EspriTLK <esprit@inbox.ru>
2023-03-09 13:01:11 +0300

    fix: init migration 5

--- database/migrations/2023.03.06T00.00.00.init.js
+++ database/migrations/2023.03.06T00.00.00.init.js
@@ -1,40 +1,73 @@
 'use strict'
 
 async function up(knex) {
-	await knex.schema.createTable('up_roles', function (table) {
-		table.increments()
-		table.string('name')
-		table.string('description')
-		table.string('type')
-		table.timestamps()
-		table.integer('created_by_id')
-		table.integer('updated_by_id')
+	const PUBLICATION_API = 'api::publication.publication'
+	const USER_PERM_PLUGIN = 'plugin::users-permissions'
+
+	await knex.schema.hasTable('up_roles').then(async function (exists) {
+		if (!exists) {
+			return await knex.schema.createTable('up_roles', function (table) {
+				table.increments('id').primary()
+				table.string('name')
+				table.string('description')
+				table.string('type')
+				table.timestamps()
+				table.integer('created_by_id')
+				table.integer('updated_by_id')
+			})
+				.then(
+					await knex.from('up_roles').insert([
+						{ name: 'Authenticated', description: 'Default role given to authenticated user.', type: 'authenticated', created_at: new Date(), updated_at: new Date() },
+						{ name: 'Public', description: 'Default role given to unauthenticated user.', type: 'public', created_at: new Date(), updated_at: new Date() },
+						// { name: 'Editor', description: 'Editor role', type: 'editor', created_at: new Date(), updated_at: new Date() },
+						// { name: 'Author', description: 'Author role', type: 'author', created_at: new Date(), updated_at: new Date() }
+					])
+				)
+		}
 	})
 
-	await knex.schema.createTable('up_permissions', function (table) {
-		table.increments()
-		table.string('action')
-		table.timestamps()
-		table.integer('created_by_id')
-		table.integer('updated_by_id')
+	await knex.schema.hasTable('up_permissions').then(async function (exists) {
+		if (!exists) {
+			return await knex.schema.createTable('up_permissions', function (table) {
+				table.increments('id').primary()
+				table.string('action')
+				table.timestamps()
+				table.integer('created_by_id')
+				table.integer('updated_by_id')
+			})
+		}
 	})
 
-	await knex.schema.createTable('up_permissions_role_links', function (table) {
-		table.increments()
-		table.integer('permission_id')
-		table.integer('role_id')
-		table.float('permission_order')
+	await knex.schema.hasTable('up_permissions_role_links').then(async function (exists) {
+		if (!exists) {
+			return await knex.schema.createTable('up_permissions_role_links', function (table) {
+				table.increments('id').primary()
+				table.integer('permission_id')
+				table.integer('role_id')
+				table.float('permission_order')
+			})
+		}
 	})
 
-	await knex.from('up_roles').insert([
-		{ name: 'Authenticated', description: 'Default role given to authenticated user.', type: 'authenticated', created_at: new Date(), updated_at: new Date() },
-		{ name: 'Public', description: 'Default role given to unauthenticated user.', type: 'public', created_at: new Date(), updated_at: new Date() },
-		{ name: 'Editor', description: 'Editor role', type: 'editor', created_at: new Date(), updated_at: new Date() },
-		{ name: 'Author', description: 'Author role', type: 'author', created_at: new Date(), updated_at: new Date() }
-	])
-
-	const authorRole = await knex.from('up_roles').where('name', 'Author');
-	const editorRole = await knex.from('up_roles').where('name', 'Editor');
+	const authorRole = await knex
+		.from('up_roles')
+		.where('name', 'Author')
+		.than(async function (exists) {
+			if (!exists) {
+				return await knex.from('up_roles').insert([
+					{ name: 'Author', description: 'Author role', type: 'author', created_at: new Date(), updated_at: new Date() }
+				])
+			}
+		});
+	const editorRole = await knex.from('up_roles')
+		.where('name', 'Editor')
+		.than(async function (exists) {
+			if (!exists) {
+				return await knex.from('up_roles').insert([
+					{ name: 'Editor', description: 'Editor role', type: 'editor', created_at: new Date(), updated_at: new Date() }
+				])
+			}
+		});;
 	const publicRole = await knex.from('up_roles').where('name', 'Public');
 	const roles = [...authorRole, ...editorRole, ...publicRole];
 
@@ -49,25 +82,25 @@ async function up(knex) {
 		.del()
 
 	const PUBLIC_ROLE_PERMISSIONS = [
-		{ action: 'plugin::users-permissions.auth.callback', created_at: new Date(), updated_at: new Date() },
-		{ action: 'api::publication.publication.find', created_at: new Date(), updated_at: new Date() },
-		{ action: 'api::publication.publication.findOne', created_at: new Date(), updated_at: new Date() },
+		{ action: `${USER_PERM_PLUGIN}.auth.callback`, created_at: new Date(), updated_at: new Date() },
+		{ action: `${PUBLICATION_API}.find`, created_at: new Date(), updated_at: new Date() },
+		{ action: `${PUBLICATION_API}.findOne`, created_at: new Date(), updated_at: new Date() },
 	]
 
 	const AUTHOR_ROLE_PERMISSIONS = [
 		...PUBLIC_ROLE_PERMISSIONS,
-		{ action: 'api::publication.publication.create', created_at: new Date(), updated_at: new Date() },
-		{ action: 'api::publication.publication.update', created_at: new Date(), updated_at: new Date() },
-		{ action: 'api::publication.publication.delete', created_at: new Date(), updated_at: new Date() },
-		{ action: 'plugin::users-permissions.auth.changePassword', created_at: new Date(), updated_at: new Date() },
-		{ action: 'plugin::users-permissions.users.find', created_at: new Date(), updated_at: new Date() },
-		{ action: 'plugin::users-permissions.users.findOne', created_at: new Date(), updated_at: new Date() },
+		{ action: `${PUBLICATION_API}.create`, created_at: new Date(), updated_at: new Date() },
+		{ action: `${PUBLICATION_API}.update`, created_at: new Date(), updated_at: new Date() },
+		{ action: `${PUBLICATION_API}.delete`, created_at: new Date(), updated_at: new Date() },
+		{ action: `${USER_PERM_PLUGIN}.auth.changePassword`, created_at: new Date(), updated_at: new Date() },
+		{ action: `${USER_PERM_PLUGIN}.user.find`, created_at: new Date(), updated_at: new Date() },
+		{ action: `${USER_PERM_PLUGIN}.user.findOne`, created_at: new Date(), updated_at: new Date() },
 	]
 
 	const EDITOR_ROLE_PERMISSIONS = [
 		...AUTHOR_ROLE_PERMISSIONS,
-		{ action: 'plugin::users-permissions.auth.register', created_at: new Date(), updated_at: new Date() },
-		{ action: 'plugin::users-permissions.users.update', created_at: new Date(), updated_at: new Date() },
+		{ action: `${USER_PERM_PLUGIN}.auth.register`, created_at: new Date(), updated_at: new Date() },
+		{ action: `${USER_PERM_PLUGIN}.user.update`, created_at: new Date(), updated_at: new Date() },
 	]
 
 	for (let role of roles) {
